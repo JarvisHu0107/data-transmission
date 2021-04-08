fc880d4975caff2b670c821664d7c38708d804b5
huxin <18162650785@163.com>
2021-04-08 22:55:24 +0800

    修改readme

--- src/main/java/com/jarvis/dts/canal/CanalServerConfig.java
+++ src/main/java/com/jarvis/dts/canal/CanalServerConfig.java
@@ -16,44 +16,23 @@ public class CanalServerConfig {
 
     private int port = 11111;
 
+    /**
+     * canal-server中单个实例对应的目录名字
+     */
     private String destination = "example";
 
     private String username = "canal";
 
     private String password = "canal";
 
+    /**
+     * 一次拉取多少数据
+     */
     private int batchSize = 1024;
 
     /**
      * 客户端订阅schema table的规则，具体参考 https://github.com/alibaba/canal/wiki/FAQ
      */
     private String clientSubscribe;
-
-    //
-    // public CanalServerConfig(String ip, int port, String destination, String username, String password) {
-    // this.ip = ip;
-    // this.port = port;
-    // this.destination = destination;
-    // this.username = username;
-    // this.password = password;
-    // }
-    //
-    //
-    // public CanalServerConfig() {
-    //
-    // }
-    //
-    // public CanalServerConfig(String ip, int port, String destination, String username, String password, int
-    // batchSize) {
-    // this(ip, port, destination, username, password);
-    // this.batchSize = batchSize;
-    // }
-    //
-    //
-    // public CanalServerConfig(String ip, int port, String destination, String username, String password, int
-    // batchSize, String clientSubscribe) {
-    // this(ip, port, destination, username, password, batchSize);
-    // this.clientSubscribe = clientSubscribe;
-    // }
 
 }
--- src/main/java/com/jarvis/dts/canal/DataTransmitTaskConfig.java
+++ src/main/java/com/jarvis/dts/canal/DataTransmitTaskConfig.java
@@ -11,11 +11,19 @@ import lombok.Data;
 public class DataTransmitTaskConfig {
 
     public static final String DEFAULT_FACTORY_ALIAS_NAME = "default";
-
+    /**
+     * canal-server的实例配置
+     */
     private CanalServerConfig serverConfig = new CanalServerConfig();
 
+    /**
+     * 策略工厂的实例名
+     */
     private String factory = DEFAULT_FACTORY_ALIAS_NAME;
 
+    /**
+     * 数据传输任务的实例名
+     */
     private String taskName;
 
 }
